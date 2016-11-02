9633c9da859a9d06619d02b647fd4272818851f0
Nick Stacey <nick@ditt.to>
2016-11-02 14:04:35 +0000

    Simple controls working

---
+++ Assets/FloorMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FloorMovement : MonoBehaviour {
+
+    private float startPos;
+    public float endPos = 10f;
+    public float speedModifier = 0.1f;
+    private float t = 0f;
+    private Rigidbody rigidBody;
+
+	public void Start ()
+    {
+        startPos = transform.position.x;
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+    public void FixedUpdate()
+    {
+        if (t <= 1) {
+            MoveFloor();
+        }
+        else {
+            SwapMovementValues();
+            t = 0f;
+        }
+	}
+
+    private void MoveFloor()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Lerp(startPos, endPos, t);
+        rigidBody.MovePosition(pos);
+
+        t += speedModifier * Time.fixedDeltaTime;
+    }
+
+    private void SwapMovementValues()
+    {
+        float temp = startPos;
+        startPos = endPos;
+        endPos = temp;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speedMultiplier = 0.1f;
+    public float jumpForce = 5f;
+    private Rigidbody rigidBody;
+    private float groundOffset;
+    private bool canJump = false;
+
+    public void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        groundOffset = GetComponent<Collider>().bounds.extents.y;
+    }
+
+	public void Update()
+    {
+        if (Input.GetButtonDown("Jump") && IsOnGround()) {
+            canJump = true;
+        }
+
+        ChangeParentGameObjectBasedOnGround();
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 newPos = transform.position + (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speedMultiplier);
+        rigidBody.MovePosition(newPos);
+        
+        if (canJump) {
+            rigidBody.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            canJump = false;
+        }
+    }
+    
+    private bool IsOnGround()
+    {
+        return GetGroundGameObject() != null;
+    }
+
+    private GameObject GetGroundGameObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, groundOffset + 0.2f)) {
+            return hit.collider.gameObject;
+        }
+
+        return null;
+    }
+
+    private void ChangeParentGameObjectBasedOnGround()
+    {
+        GameObject ground = GetGroundGameObject();
+        if (ground == null) {
+            return;
+        }
+        
+        if (transform.parent == null || transform.parent.gameObject != ground) {
+            transform.parent = ground.transform;
+        }
+    }
+}
